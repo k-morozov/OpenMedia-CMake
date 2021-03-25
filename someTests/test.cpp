c402c3cf973553b861d6c723f4b0b67342c40d8b
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-03-25 08:07:26 +0300

    add CI and fix tests

--- someTests/test.cpp
+++ someTests/test.cpp
@@ -3,7 +3,14 @@
 //
 
 #include "gtest/gtest.h"
+#include "someLib.h"
 
 TEST(CoreTest, ExampleTest1) {
     ASSERT_TRUE(true);
+}
+
+TEST(CoreTest, ExampleTest2) {
+    const auto expected = "Simple message from someLibDir";
+    const auto actual = someLib::getMessage();
+    EXPECT_STREQ(actual.c_str(), expected);
 }
